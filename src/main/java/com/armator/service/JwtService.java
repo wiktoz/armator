a72a9e28c8c3e342f8c44ca6a034d0c11e03a8b1
adi7312 <114097246+adi7312@users.noreply.github.com>
2024-01-06 19:20:03 +0100

    perf: embedding role claim in jwt token

--- src/main/java/com/armator/service/JwtService.java
+++ src/main/java/com/armator/service/JwtService.java
@@ -33,6 +33,7 @@ public class JwtService {
         return Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
+                .claim("role", userDetails.getAuthorities().toArray()[0].toString())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
