81b16f349d03f4cff7110cac01ef7de9068ecc73
wiktoz <wiciazaw@wp.pl>
2024-01-07 18:53:37 +0100

    Updated map and design

--- app/api/token/set/route.ts
+++ app/api/token/set/route.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers"
 
-export async function POST(req: Request, res: Response){
+export async function POST(req: Request){
     const body = await req.json()
 
     cookies().set({
