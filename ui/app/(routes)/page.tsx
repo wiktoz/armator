7e01aaef14d73cf3b6bedbe3fba38055cf6ca7ff
wiktoz <wiciazaw@wp.pl>
2024-01-19 17:08:53 +0100

    fix: updated Dockerfile

--- ui/app/(routes)/page.tsx
+++ ui/app/(routes)/page.tsx
@@ -1,8 +1,9 @@
-import Datepicker from "@/app/_components/form/Datepicker";
-
 const Home = () => {
     return(
-        <Datepicker/>
+        <div className={"flex flex-col text-center"}>
+            <p className={"text-2xl font-bold text-gray-800"}>Shipowner</p>
+            <p className={"text-sm text-gray-500"}>Manage your fleet</p>
+        </div>
     )
 }
 
